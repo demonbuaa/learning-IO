49c1659cf381a3a887d3cd76048565800fe6f030
chenzhi_linux <chenzhigz@163.com>
2020-09-06 12:32:16 +0800

    fin fout cin cout experiment

--- fileio.cpp
+++ fileio.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+
+using namespace std;
 
 int main()
 {
-    
+    ofstream fout("chenzhi.txt", ios::app);
+
+    char input;
+
+    cin >> noskipws >> input;
+    while(1)
+    {
+       fout << input;
+       cin >> input;
+       cout << input;
+       if(input == ':')
+       {
+          cout << "quit" << endl;
+          break;
+       }
+    }
+    fout << flush;
+    fout.close();
+
     return 0;
 }
